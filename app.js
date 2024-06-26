b2803eaae89fb42062d9559a251b722b8754d00c
gnyrd <2048411+gnyrd@users.noreply.github.com>
2024-06-25 21:29:46 -0600

    Update app.js
    Customized log entry

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`Park City Contact app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
